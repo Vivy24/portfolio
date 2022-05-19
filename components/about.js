07695c623e720298a3cfd0a65a8d692485c209a3
Vivy Vuong <bvvuong@myseneca.ca>
2022-05-18 22:40:35 -0400

    Portfolio v2-changes to deploy

--- components/about.js
+++ components/about.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import style from "../styles/about.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
 import { faHandPeace } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+import Image from "next/image";
 
 const About = () => {
   const [more, setMore] = useState(false);
@@ -17,7 +18,7 @@ const About = () => {
         <h1>Hello</h1>
       </div>
       <div className={style.content}>
-        <img alt="vivyimg" src="image/vyvy.png" />
+        <Image alt="vivyimg" src="/image/vyvy.png" width={250} height={250} />
 
         <h2 data-testid="title">
           Vivy Vuong <br />
@@ -29,26 +30,27 @@ const About = () => {
             style={{ fontSize: "25px" }}
             icon={faHandPeace}
           ></FontAwesomeIcon>{" "}
-          ,
-          <br /> My name is Vivy Vuong. I'm a Computer Science student, who
-          passionate about becoming a web developer. My coding journey begin
-          when I was in grade 11. In grade 11, I took the Introduction to
-          Computer because everyone told me it is an easy course. Also, I'm very
-          good at Math at that time , so I thought I would do well in that
-          class. Eventually, I fell in love with coding after this class. I love
-          spending time designing and finding the best solution to solve the
-          problem.
+          &apos;
+          <br /> My name is Vivy Vuong. I&quot;m a Computer Science
+          student&apos; who passionate about becoming a web developer. My coding
+          journey begin when I was in grade 11. In grade 11&apos; I took the
+          Introduction to Computer because everyone told me it is an easy
+          course. Also&apos; I&quot;m very good at Math at that time &apos; so I
+          thought I would do well in that class. Eventually&apos; I fell in love
+          with coding after this class. I love spending time designing and
+          finding the best solution to solve the problem.
           {more ? (
             <span className={style.readMore} data-testid="more">
-              Then, during the covid time, I'm learning code as a hobby in my
-              free time. I'm self-learning through many online materials such as
-              Youtube, FreeCodeCamp, Udemy, etc. After learning a programming
-              language, I asked myself which industry I should focus on? After a
-              week of researching, I decided to become a web developer because I
-              think that website is the most powerful tool for us, developers,
-              to "talk" to the world since everyone can easily get access to the
-              web. This is how my journey to become a good programmer and a good
-              web developer begins.
+              Then&apos; during the covid time&apos; I&quot;m learning code as a
+              hobby in my free time. I&quot;m self-learning through many online
+              materials such as Youtube&apos; FreeCodeCamp&apos; Udemy&apos;
+              etc. After learning a programming language&apos; I asked myself
+              which industry I should focus on? After a week of
+              researching&apos; I decided to become a web developer because I
+              think that website is the most powerful tool for us&apos;
+              developers&apos; to &quot;talk&quot; to the world since everyone
+              can easily get access to the web. This is how my journey to become
+              a good programmer and a good web developer begins.
             </span>
           ) : (
             <span className={style.dots} data-testid="less">
--- components/projectDes.js
+++ components/projectDes.js
@@ -1,4 +1,5 @@
 import style from "../styles/projectDesc.module.css";
+import Image from "next/image";
 const ProjectDesc = ({
   name,
   des,
@@ -13,7 +14,13 @@ const ProjectDesc = ({
       data-testid="project"
       className={`${style.projectCard} ${right && style.right}`}
     >
-      <img alt="projectImage" src={image} />
+      <Image
+        className={style.img}
+        alt="projectImage"
+        src={`/${image}`}
+        width={1000}
+        height={500}
+      />
       <div className={style.projectContent}>
         <h4>{name}</h4>
         <p data-testid="description" style={{ textAlign: "justify" }}>
@@ -30,12 +37,13 @@ const ProjectDesc = ({
           })}
         </p>
 
-        <a data-testid="link" href={github} target="_blank">
+        <a data-testid="link" href={github} target="_blank" rel="noreferrer">
           Github
         </a>
         {publicLink && (
           <a
             data-testid="link"
+            rel="noreferrer"
             target="_blank"
             style={{ marginLeft: "3px" }}
             href={publicLink}
